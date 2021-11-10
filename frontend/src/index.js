2b5ad3fcea8f09f3171336b3d53a69eb4bfc7103
Jin Chi He <hejinchi@cn.ibm.com>
2021-11-10 10:04:36 +0800

    Enhance deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 const title = 'My Minimal React Webpack Babel Setup';
 
 window._env_ = {
-  SERVER_URL: "http://9.30.189.42:8888/",
-  REPORT_URL: "http://9.30.189.42:8889/",
+  SERVER_URL: "http://9.30.189.42:8889/",
+  REPORT_URL: "http://9.30.189.42:8888/",
 }
 
 ReactDOM.render(
